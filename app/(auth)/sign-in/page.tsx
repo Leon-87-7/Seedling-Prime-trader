dd9daee7a1d7d505576514486eb4c8deaf4ba5b9
LeonEidelman <leoneidelman09@gmail.com>
2025-11-14 15:52:19 +0200

    sign-in implemented

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,80 @@
-import React from 'react';
+'use client';
+
+import FooterLink from '@/components/forms/FooterLink';
+import InputField from '@/components/forms/InputField';
+import { Button } from '@/components/ui/button';
+import { useForm } from 'react-hook-form';
 
 const SignIn = () => {
-  return <div>innn</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInFormData>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onBlur',
+  });
+  const onSubmit = async (data: SignInFormData) => {
+    try {
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return (
+    <>
+      <h1 className="form-title">Log In your account</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="space-y-5"
+      >
+        {/* Input fields will go here */}
+
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="john.doe@example.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: 'Email is required',
+            pattern: {
+              value: /^\w+@\w+\.\w+$/,
+              message: 'Invalid email address',
+            },
+          }}
+        />
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter a strong password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{
+            required: 'Password is required',
+            minLength: 8,
+          }}
+        />
+
+        <Button
+          type="submit"
+          className="yellow-btn w-full mt-5"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? 'Logging In...' : 'Log In'}
+        </Button>
+        <FooterLink
+          text="Don't have an account yet?"
+          linkText="Sign Up"
+          href="/sign-up"
+        />
+      </form>
+    </>
+  );
 };
 
 export default SignIn;
