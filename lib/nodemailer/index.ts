2308d0f037a33c110858c32baa12838c380e0325
LeonEidelman <leoneidelman09@gmail.com>
2025-11-19 16:09:31 +0200

    implement daily news summary

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,5 +1,8 @@
 import nodemailer from 'nodemailer';
-import { WELCOME_EMAIL_TEMPLATE } from './templates';
+import {
+  NEWS_SUMMARY_EMAIL_TEMPLATE,
+  WELCOME_EMAIL_TEMPLATE,
+} from './templates';
 
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -61,4 +64,29 @@ export const sendWelcomeEmail = async ({
       }`
     );
   }
+};
+
+export const sendNewsSummaryEmail = async ({
+  email,
+  date,
+  newsContent,
+}: {
+  email: string;
+  date: string;
+  newsContent: string;
+}): Promise<void> => {
+  const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE.replace(
+    '{{date}}',
+    date
+  ).replace('{{newsContent}}', newsContent);
+
+  const mailOptions = {
+    from: `"SeedlingPrime News" <${process.env.NODEMAILER_EMAIL}>`,
+    to: email,
+    subject: `Market News Summary Today - ${date}`,
+    text: `Today's market news summary from SeedlingPrime`,
+    html: htmlTemplate,
+  };
+
+  await transporter.sendMail(mailOptions);
 };
